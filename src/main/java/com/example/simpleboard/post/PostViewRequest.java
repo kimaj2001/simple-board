fffc8683f24de7fff1a6495f5646e54db95bd6eb
kimeojin <kimaj2001@gmail.com>
2023-11-01 01:18:00 +0900

    Feat: "PostViewRequest 생성"
    - 비회원이 자신이 작성한 게시글을 확인하고자 할 때 비밀번호를 입력해야 하기 때문에 Post 방식으로 받아야 하는데 이를 위해 해당 클래스 생성

---
+++ src/main/java/com/example/simpleboard/post/PostViewRequest.java
@@ -0,0 +1,21 @@
+package com.example.simpleboard.post;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostViewRequest {
+    @NotNull
+    private Long postId;
+    @NotBlank
+    private String password;
+}
